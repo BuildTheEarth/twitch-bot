82abec6289797f750899f97d75c79c1976fac800
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2021-05-23 18:10:22 -0700

    chore(utils): remember that this is python and make it valid code

--- utils.py
+++ utils.py
@@ -3,9 +3,9 @@ class Config:
 	def __init__(self):
 		with open("config.json") as e:
 			conf = json.load(e)
-			this.prefix = conf['prefix']
-			this.db_url = conf['db_url']
+			self.prefix = conf['prefix']
+			self.db_url = conf['db_url']
 	def prefix(self):
-		return this.prefix
+		return self.prefix
 	def db_url(self):
-		return this.db_url
+		return self.db_url
